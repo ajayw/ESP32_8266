4bb8c9bfe17ca10c635983c071d5691481adbe8e
ajayw <ajay_w@qq.com>
2025-02-24 01:48:10 +0800

    ADD PRO

---
+++ HomeSystem/HomeSystem.h
@@ -0,0 +1,17 @@
+// 图标字节数据（这些数据可以通过在线工具image2cpp生成）
+#ifndef HOMESYSTEM_H
+#define HOMESYSTEM_H
+
+// '浓云', 40x40px 
+extern const uint8_t cloudIcon[] PROGMEM;
+
+// '晴天', 40x40px
+extern const uint8_t sunIcon[] PROGMEM;
+
+// '温度', 40x40px
+extern const uint8_t tempIcon[] PROGMEM;
+
+// '雨天', 40x40px
+extern const uint8_t rainIcon[] PROGMEM;
+
+#endif // HOMESYSTEM_H
